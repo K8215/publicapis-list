621d2c8f504af0e98f5d24440c6202644f4f499c
Kaitlyn Coleman <k8lak8@protonmail.com>
2024-02-25 13:45:07 -0500

    hid base path for production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	cache: false,
-	base: "/projects/public-apis/",
+	//cache: false,
+	//base: "/projects/public-apis/",
 });
